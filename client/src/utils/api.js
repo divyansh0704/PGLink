e266503b58241dab6482e2ea8aeb68c5859d83b9
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-07-30 11:09:12 +0530

    backend api change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import  axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3009/api',
+    baseURL: 'https://pglink.onrender.com/api',
 })
 
 API.interceptors.request.use((config) => {
