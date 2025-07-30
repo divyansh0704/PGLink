cb97b3400a78812d17de752039d7f6364870294d
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-07-30 11:24:53 +0530

    cors setup

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 require("dotenv").config();
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://pglink.onrender.com',
     credentials:true
 }))
 
