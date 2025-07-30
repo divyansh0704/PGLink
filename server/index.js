b6fe0e161a9b614b6372dd9a04f112178399f50e
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-07-30 11:45:52 +0530

    yes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const {initDB} = require("./models");
+const { initDB } = require("./models");
 const userRoutes = require("./routes/userRoutes");
 const pgRoutes = require("./routes/pgRoutes")
 const paymentRoutes = require('./routes/paymentRoutes');
 const cors = require('cors');
 const path = require("path");
 
+const PORT = process.env.PORT || 3009;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://pg-link.vercel.app"
+];
 
 
 const app = express();
@@ -14,24 +19,30 @@ const app = express();
 require("dotenv").config();
 app.use(express.json())
 app.use(cors({
-    origin: 'https://pglink.onrender.com',
-    credentials:true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
 }))
 
 initDB();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use("/api/users",userRoutes);
-app.use("/api/pgs",pgRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/pgs", pgRoutes);
 app.use('/api/payment', paymentRoutes);
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("PGLInk backend running 🚀")
 })
 
-app.listen(3009,()=>{
-    console.log("Server started on http://localhost:3009")
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`)
 })
 
 
