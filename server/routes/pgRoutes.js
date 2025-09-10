0cd6fc957bfc6bf41980ef16f6f611db450c8b23
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-09-10 15:29:17 +0530

    file size

--- server/routes/pgRoutes.js
+++ server/routes/pgRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {protect}= require("../middleware/authMiddleware");
-const {createPG,getPGById,getAllPGs,getPGByOwner, deletePG, updatePG, unlockedPG, allPGs, limited}=require("../controllers/pgController");
+const { protect } = require("../middleware/authMiddleware");
+const { createPG, getPGById, getAllPGs, getPGByOwner, deletePG, updatePG, unlockedPG, allPGs, limited } = require("../controllers/pgController");
 const multer = require('multer');
 const path = require("path");
-const {storage} = require("../utils/cloudinary");
+const { storage } = require("../utils/cloudinary");
 
 // const storage = multer.diskStorage({
 //     destination: (req, file, cb) => {
@@ -15,19 +15,22 @@ const {storage} = require("../utils/cloudinary");
 //     },
 // })
 
-const upload = multer({storage});
+const upload = multer({
+    storage,
+    limits: { fileSize: 15 * 1024 * 1024 }
+});
 
-router.post("/",protect,upload.single('photo'),createPG);
-router.get("/",getAllPGs);
-router.get("/my-pgs",protect,getPGByOwner);
-router.get('/unlocked', protect,unlockedPG) ;
+router.post("/", protect, upload.single('photo'), createPG);
+router.get("/", getAllPGs);
+router.get("/my-pgs", protect, getPGByOwner);
+router.get('/unlocked', protect, unlockedPG);
 
-router.get('/limited',protect,limited)
-router.get('/all',protect,allPGs)
+router.get('/limited', protect, limited)
+router.get('/all', protect, allPGs)
 
-router.delete("/:id",protect,deletePG);
-router.get("/:id",protect,getPGById);
-router.put("/:id",protect,updatePG);
+router.delete("/:id", protect, deletePG);
+router.get("/:id", protect, getPGById);
+router.put("/:id", protect, updatePG);
 
 
 
