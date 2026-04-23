f45d2cb7f504f49e9e530a7d0d9796ce57ba2e8c
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-04-23 15:28:55 +0530

    delete applied in admin panel for colleges

--- server/controllers/collegeController.js
+++ server/controllers/collegeController.js
@@ -36,3 +36,19 @@ exports.getAllColleges = async (req, res) => {
  
     }
 }
+exports.deleteCollege = async(req,res)=>{
+    try{
+        const collegeId = req.params.id;
+        const college = await College.findByPk(collegeId);
+        if(!college){
+            res.status(404).json({error:"college not found"})
+        }
+
+        await college.destroy();
+        res.status(200).json({message:"college deleted successfully",college})
+
+    }catch(err){
+        res.status(500).json({error:err.message})
+    
+    }
+}
